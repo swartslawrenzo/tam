6331983adf3015fe4d36eb69b7eb66f585ff4574
Lawrenzo Swarts <106303144+swartslawrenzo@users.noreply.github.com>
2024-10-02 20:34:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMessage() {
+    document.getElementById('message-popup').classList.remove('hidden');
+}
+
+function closeMessage() {
+    document.getElementById('message-popup').classList.add('hidden');
+}
